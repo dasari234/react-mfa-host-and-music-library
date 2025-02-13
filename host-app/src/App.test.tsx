13360949af536f848b8bcd4ce105b205e4d1db44
srinivas <dasari9@gmail.com>
2025-02-13 18:59:40 +0530

    fixed test case failures

--- host-app/src/App.test.tsx
+++ host-app/src/App.test.tsx
@@ -1,19 +1,13 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
-
-// Mock the remote module
-// jest.mock('musicLibrary/App', () => ({
-//   __esModule: true,
-//   default: () => <div>Music Library Content</div>,
-// }));
 
 describe("App Component", () => {
   it("renders the layout and music library content", async () => {
     render(<App />);
 
-    expect(screen.getByText("Music Library")).toBeInTheDocument();
-    // Wait for the lazy-loaded component
-    const content = await screen.findByText("Music Library");
-    expect(content).toBeInTheDocument();
+    // Wait for the lazy-loaded "Music Library" text to appear
+    await waitFor(() => {
+      expect(screen.getByText("Music Library")).toBeInTheDocument();
+    });
   });
 });
